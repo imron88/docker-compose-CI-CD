fc21bcf45b70fa0a923590ee1960e8f7692dc4c1
ronak <ronakpraja340@gmail.com>
2026-01-26 02:14:36 +0530

    user get

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import { PrismaClient } from "../generated/prisma/client.js";
 
 const app = express();
+const prisma = new PrismaClient();
 const port = 3000; 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-      "message":"Hello World!"
-  })
+app.get("/", async (req, res) => {
+    const user = await prisma.user.findMany();
+    res.json({
+        "Data": user
+    })
 });
 
 app.post("/",(req,res)=>{
