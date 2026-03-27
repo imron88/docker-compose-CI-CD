dc246e68be15daa1f944bc65b21a1a0cf2e7abd6
ronak <ronakpraja340@gmail.com>
2026-03-27 21:20:41 +0530

    debugs

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "../generated/prisma/client.js";
 const app = express();
 const prisma = new PrismaClient();
 const port = 3000; 
-app.use(express.json());
+app.use(express.json({}));
 
 app.get("/", async (req, res) => {
     const user = await prisma.user.findMany();
